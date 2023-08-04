50754efe0214ceca73d8a5ec4487f62a4d618c75
eMKa <maciej.qb@gmail.com>
2023-08-04 23:16:09 +0200

    add full build script and small cleanup

---
+++ full_build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+echo "Project build start"
+
+set -x # Debug mode
+set -e # Exit immediately if any command fail
+
+ROOT_BUILD_DIR=build_pack
+UNIT_TESTS_BUILD_DIR=$ROOT_BUILD_DIR/build_unit_tests
+PROD_BUILD_DIR=$ROOT_BUILD_DIR/build_prod
+
+MASTER_CMAKE_PROJECT_NAME="AwesomeEsp32DevelopmentEnv"
+
+build_unit_tests()
+{
+    cmake -B $UNIT_TESTS_BUILD_DIR -G Ninja . -DUNIT_TEST_BUILD="ON" -DMASTER_CMAKE_PROJECT_NAME=$MASTER_CMAKE_PROJECT_NAME
+    cmake --build $UNIT_TESTS_BUILD_DIR
+}
+
+build_production_code()
+{
+    cmake -B $PROD_BUILD_DIR -G Ninja . -DUNIT_TEST_BUILD="OFF" -DMASTER_CMAKE_PROJECT_NAME=$MASTER_CMAKE_PROJECT_NAME
+    cmake --build $PROD_BUILD_DIR
+
+}
+
+main()
+{
+    build_unit_tests
+    ./$UNIT_TESTS_BUILD_DIR//main/$MASTER_CMAKE_PROJECT_NAME
+    build_production_code
+}  
+
+main
